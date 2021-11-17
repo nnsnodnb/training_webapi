c1bdb011053fe73bb8c901ecd20a3ad5fdb4d297
Yuya Oka <nnsnodnb@gmail.com>
2021-11-18 01:25:30 +0900

    Configure Task model

--- db/admin.py
+++ db/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.tasks import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "title",
+        "status",
+        "user",
+        "created_at",
+    )
+    list_display_links = (
+        "id",
+        "title",
+        "status",
+    )
+    list_filter = ("status", "user")
+    list_select_related = ("user",)
--- db/models/tasks.py
+++ db/models/tasks.py
@@ -4,10 +4,16 @@ from django.utils import timezone
 
 
 class Task(models.Model):
+    class StatusChoices(models.TextChoices):
+        BACKLOG = "backlog" "バックログ"
+        IN_PROGRESS = "in_progress", "進行中"
+        IN_REVIEWS = "in_reviews", "レビュー中"
+        FINISHED = "finished", "完了"
 
     title = models.CharField("タイトル", max_length=300)
-    thumbnails = models.CharField("サムネイル", max_length=500, null=True, blank=True)
+    thumbnail = models.CharField("サムネイルID", max_length=500, null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField("ステータス", max_length=12, default=StatusChoices.BACKLOG, choices=StatusChoices.choices)
     created_at = models.DateTimeField("登録日時", default=timezone.now, blank=True)
     updated_at = models.DateTimeField("更新日時", default=timezone.now, blank=True)
 
