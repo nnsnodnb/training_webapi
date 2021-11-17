37d95cc7b0fb00228aca6c3993d37f2133937f78
Yuya Oka <nnsnodnb@gmail.com>
2021-11-18 00:26:39 +0900

    Add webapi.views.tasks.list_create

---
+++ webapi/views/tasks/list_create.py
@@ -0,0 +1,9 @@
+from rest_framework.generics import ListCreateAPIView
+
+
+class TaskListCreateAPIView(ListCreateAPIView):
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
