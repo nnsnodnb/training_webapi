280ff8d63788ef5f59b1eed37a9312a24b093d17
Yuya Oka <nnsnodnb@gmail.com>
2021-11-23 23:00:40 +0900

    Add CommentAdmin

--- db/admin.py
+++ db/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 
-from .models.tasks import Task
+from .models import Comment, Task
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "content",
+        "image_ids",
+        "user",
+        "task",
+        "created",
+    )
+    list_display_links = (
+        "id",
+        "content",
+    )
+    list_filter = (
+        "user",
+        "task",
+    )
+    list_select_related = (
+        "user",
+        "task",
+    )
 
 
 @admin.register(Task)
