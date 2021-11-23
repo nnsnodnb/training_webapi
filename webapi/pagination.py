278df0e39f7b0459a9df9a92905b112f4b8b7490
Yuya Oka <nnsnodnb@gmail.com>
2021-11-23 23:00:51 +0900

    Run black

--- webapi/pagination.py
+++ webapi/pagination.py
@@ -11,12 +11,12 @@ class CursorPagination(BaseCursorPagination):
     def encode_cursor(self, cursor):
         tokens = {}
         if cursor.offset != 0:
-            tokens['o'] = str(cursor.offset)
+            tokens["o"] = str(cursor.offset)
         if cursor.reverse:
-            tokens['r'] = '1'
+            tokens["r"] = "1"
         if cursor.position is not None:
-            tokens['p'] = cursor.position
+            tokens["p"] = cursor.position
 
         querystring = parse.urlencode(tokens, doseq=True)
-        encoded = b64encode(querystring.encode('ascii')).decode('ascii')
+        encoded = b64encode(querystring.encode("ascii")).decode("ascii")
         return encoded
